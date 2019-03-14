4485a5979f69679e73fe90556da145523de23c73
kinloketan@gmail.com <kinloketan@gmail.com>
2019-03-14 19:43:30 +0800

    Part of form done

--- index.php
+++ index.php
@@ -6,13 +6,38 @@
     <title>Sistem Aset Bilik i-CreatorZ</title>
     <link rel="icon" href="icon.png" type="image/png" sizes="any"> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
-    <script src="main.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="../SK_Project/Assets/index.css">
+    <script src="../SK_Project/Assets/main.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-blue.min.css" /> 
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-light_blue.min.css" /> 
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 </head>
 <body>
-    
+
+    <!-- Form -->
+    <form method="post" class="form-box">
+    <div class="form-components">
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <!-- Username -->
+            <input class="mdl-textfield__input" type="text" id="username" name="username">
+            <label class="mdl-textfield__label" for="username">Username</label>
+        </div>
+        <br>
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <!-- Password -->
+            <input class="mdl-textfield__input" type="password" id="password" name="password">
+            <label class="mdl-textfield__label" for="password">Password</label>
+        </div>
+        <br>
+        <!-- Login Button -->
+        <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+            Login
+        </button>
+        <br><br>
+        <!-- Register dialog -->
+        <a>Belum daftar?</a><a href="">Daftar sekarang</a>
+    </div>
+    </form>
+
 </body>
 </html>
