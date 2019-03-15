b1b74057435de9460897e01511770f77939cbb86
kinloketan@gmail.com <kinloketan@gmail.com>
2019-03-15 15:45:41 +0800

    Login page done

--- Assets/main.js
+++ Assets/main.js
@@ -1,10 +1,47 @@
 function onLoad() {
     $("#Title").fadeIn("slow");
-    // $("#form-box").slideDown("slow");
-    $(".form-box").addClass("show");
-    // setTimeout($(".form-components").addClass("show"),4000);
-    setTimeout(function() {
-        $(".form-components").addClass("show")
-    },1000);
+    LoginBoxOn();
     document.getElementById("Title").style.display = "block";
+}
+
+function LoginBoxOn() {
+    $(".login-form-box").addClass("show");
+    setTimeout(function() {
+        $(".login-form-components").addClass("show")
+    },1000);
+}
+
+function LoginBoxOff() {
+    $(".login-form-components").removeClass("show");
+    setTimeout(function() {
+        $(".login-form-box").removeClass("show");
+    },420);
+}
+
+function RegisterBoxOn() {
+    $(".register-form-box").addClass("show");
+    setTimeout(function() {
+        $(".register-form-components").addClass("show");
+    },1000);
+}
+
+function RegisterBoxOff() {
+    $(".register-form-components").removeClass("show");
+    setTimeout(function() {
+        $(".register-form-box").removeClass("show");
+    }, 420);
+}
+
+function registerButtonPressed() {
+    LoginBoxOff();
+    setTimeout(function() {
+        RegisterBoxOn();
+    },500);
+}
+
+function BackToLoginBox() {
+    RegisterBoxOff();
+    setTimeout(function() {
+        LoginBoxOn();
+    },500);
 }
--- index.php
+++ index.php
@@ -6,39 +6,69 @@
     <title>Sistem Aset Bilik i-CreatorZ</title>
     <link rel="icon" href="icon.png" type="image/png"> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="../SK_Project/Assets/index.css">
-    <script src="../SK_Project/Assets/main.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="./Assets/index.css">
+    <script src="./Assets/main.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-light_blue.min.css" /> 
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-blue.min.css" /> 
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 </head>
 <body onload="onLoad()">
     <a class="Title" id="Title">Sistem Aset Bilik i-CreatorZ</a>
-    <!-- Form -->
-    <form method="post" class="form-box" id="form-box">
-    <div class="form-components" id="form-components">
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" >
-            <!-- Username -->
-            <input class="mdl-textfield__input" type="text" id="username" name="username">
-            <label class="mdl-textfield__label" for="username">Username</label>
+    <!-- Login Form -->
+    <form method="post">
+    <div class="login-form-box" id="login-form-box">
+        <div class="login-form-components">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <!-- Username -->
+                <input class="mdl-textfield__input" type="text" id="login-username" name="login-username">
+                <label class="mdl-textfield__label" for="login-username">Username</label>
+            </div>
+            <br>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <!-- Password -->
+                <input class="mdl-textfield__input" type="password" id="login-password" name="login-password">
+                <label class="mdl-textfield__label" for="login-password">Password</label>
+            </div>
+            <br>
+            <!-- Login Button -->
+            <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent login-button" id="login-button">
+                Login
+            </button>
+            <br><br>
+            <!-- Register dialog -->
+            <a class="RegisterDialog">Belum daftar?</a><button class="register-button" type="button" onclick="registerButtonPressed()"><a>Daftar sekarang</a></button>
         </div>
-        <br>
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-            <!-- Password -->
-            <input class="mdl-textfield__input" type="password" id="password" name="password">
-            <label class="mdl-textfield__label" for="password">Password</label>
-        </div>
-        <br>
-        <!-- Login Button -->
-        <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent login-button" id="login-button">
-            Login
-        </button>
-        <br><br>
-        <!-- Register dialog -->
-        <a class="RegisterDialog">Belum daftar?</a><a href="">Daftar sekarang</a>
     </div>
+    <!-- Register Form -->
+        <div class="register-form-box">
+            <div class="register-form-components">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <!-- Username -->
+                    <input class="mdl-textfield__input" type="text" id="register-username" name="register-username">
+                    <label class="mdl-textfield__label" for="register-username">Username</label>
+                </div>
+                <br>
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <!-- Password -->
+                    <input class="mdl-textfield__input" type="password" id="register-password" name="register-password">
+                    <label class="mdl-textfield__label" for="register-password">Password</label>
+                </div>
+                <br>
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <!-- Confirm Password -->
+                    <input class="mdl-textfield__input" type="password" id="register-confirm-password" name="register-confirm-password">
+                    <label class="mdl-textfield__label" for="register-confirm-password">Confirm Password</label>
+                </div>
+                <br><br>
+                <!-- Login Button -->
+                <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent login-button" id="register-button">
+                    Register
+                </button>
+                <br><br>
+                <button type="button" class="back-login-box" onclick="BackToLoginBox()"><a>Back to Login</a></button>
+            </div>
+        </div>
     </form>
-
 </body>
 </html>
